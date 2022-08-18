e84f624e0daf808f2f40b01793aa90683c57632a
Angel Sandoval <sandovalangel10@hotmail.com>
2022-08-18 03:24:23 -0700

    Updated JS and CSS

---
+++ assets/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does DOM stand for?",
+    answer: "Document Object Model",
+    options: [
+        "Data Object Markup",
+        "Document Object Markup",
+        "Data Object Model",
+        "Document Object Model"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What are JavaScript Data Types?",
+    answer: "All of the Above",
+    options: [
+      "String",
+      "Object",
+      "Number",
+      "All of the Above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How do you declare a JavaScript variable?",
+    answer: "var carName;",
+    options: [
+      "var carName;",
+      "v carName;",
+      "variable carName;",
+      "variable = carName;"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which operator is used to assign a value to a variable?",
+    answer: "=",
+    options: [
+      "-",
+      "=",
+      "X",
+      "*"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which event occurs when the user clicks on an HTML element?",
+    answer: "onclick",
+    options: [
+      "onmouseclick",
+      "onclick",
+      "onchange",
+      "onmouseover"
+    ]
+  },
+];
